ae426aa0fc1c8c22ced504f8905506ebd6040838
levent97 <157998172+levent97@users.noreply.github.com>
2024-05-11 23:26:56 -0700

    Add files via upload

--- picarx/man.py
+++ picarx/man.py
@@ -21,15 +21,16 @@ if __name__ == "__main__":
         while True:
             key = readchar.readkey()
             key = key.lower()
-                if 'q' == key:
-                    print('forward and left.')
-                    px.set_dir_servo_angle(-15)
-                elif 'w' == key:
-                    print('forward.')
-                    px.set_dir_servo_angle(0)
-                elif 'e' == key:
-                    print('forward and right.')
-                    px.set_dir_servo_angle(15)
+            
+            if 'q' == key:
+                print('forward and left.')
+                px.set_dir_servo_angle(-15)
+            elif 'w' == key:
+                print('forward.')
+                px.set_dir_servo_angle(0)
+            elif 'e' == key:
+                print('forward and right.')
+                px.set_dir_servo_angle(15)
           
             elif key == readchar.key.CTRL_C:
                 print("\n Quit")
