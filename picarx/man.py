b7e28932174ea21d7a5fd2434117283d6bc60eb4
levent97 <157998172+levent97@users.noreply.github.com>
2024-05-11 23:11:56 -0700

    Add files via upload

---
+++ picarx/man.py
@@ -0,0 +1,45 @@
+from picarx_improved import Picarx
+from time import sleep
+import readchar
+
+manual = '''
+Press keys on keyboard to control PiCar-X!
+    q: Forward and left
+    w: Forward
+    e: Forward and right
+    ctrl+c: Press twice to exit the program
+'''
+
+
+if __name__ == "__main__":
+    try:
+        pan_angle = 0
+        tilt_angle = 0
+        px = Picarx()
+        print(manual)
+        px.forward(50)
+        while True:
+            key = readchar.readkey()
+            key = key.lower()
+                if 'q' == key:
+                    print('forward and left.')
+                    px.set_dir_servo_angle(-15)
+                elif 'w' == key:
+                    print('forward.')
+                    px.set_dir_servo_angle(0)
+                elif 'e' == key:
+                    print('forward and right.')
+                    px.set_dir_servo_angle(15)
+          
+            elif key == readchar.key.CTRL_C:
+                print("\n Quit")
+                break
+
+    finally:
+        px.set_cam_tilt_angle(0)
+        px.set_cam_pan_angle(0)  
+        px.set_dir_servo_angle(0)  
+        px.stop()
+        sleep(.2)
+
+
